dc9c858901e43f0133a8608024f4834dd97adfd8
Taeuk Gwak <gwak2837@kakao.com>
2023-10-13 21:07:59 +0900

    fix: build 오류

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,14 @@
 import { kv } from '@vercel/kv'
 
 export async function GET() {
-  return 'Hello world!'
+  return Response.json({ data: 'Hello world!' })
 }
 
 export async function POST() {
   try {
     const data = await kv.set('user_1_session', '123abc', { ex: 100, nx: true })
-    return Response.json(data)
+    return Response.json({ data })
   } catch (error) {
-    return Response.json(error)
+    return Response.json({ error })
   }
 }
