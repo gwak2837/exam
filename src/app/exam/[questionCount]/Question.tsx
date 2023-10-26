7acbc198743d3a76de63874535a7ea3ef35591ca
Taeuk Gwak <gwak2837@kakao.com>
2023-10-26 18:30:03 +0900

    문제 ui 적용

--- src/app/exam/[questionCount]/Question.tsx
+++ src/app/exam/[questionCount]/Question.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useAnswerStore, useQuestionStore } from '@/app/exam/[questionCount]/zustand'
+import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 
 type Props = {
@@ -11,17 +12,31 @@ export default function Question({ questions: newQuestions }: Props) {
   const { questions, setQuestions } = useQuestionStore()
   const { answers, addAnswer } = useAnswerStore()
 
-  const newQuestionIds = newQuestions?.map((q) => q.id).toString()
+  const newQuestionIds = newQuestions?.map((q) => q.id)
 
   useEffect(() => {
     if (questions.length > 0) return
     setQuestions(newQuestions)
-  }, [newQuestionIds])
+  }, [newQuestionIds.toString()])
+
+  const searchParams = useSearchParams()
+  const i = +(searchParams.get('i') ?? 1)
+  const question = questions[i - 1]
+
+  function selectAnswer() {}
 
   return (
     <>
-      <button onClick={() => addAnswer([1, 2])}>asdf</button>
-      <pre className="overflow-x-scroll">{JSON.stringify({ answers, newQuestions, questions }, null, 2)}</pre>
+      <div className="flex grow items-center justify-center">
+        <p className="max-w-prose text-center text-xl md:text-2xl">{question?.문제}</p>
+      </div>
+      <form className="grid gap-4" onSubmit={(e) => e.preventDefault()}>
+        {question?.선택지.map((선택지) => (
+          <button key={선택지.id} className="whitespace-nowrap border p-4 md:text-lg" onClick={selectAnswer}>
+            {선택지.label}
+          </button>
+        ))}
+      </form>
     </>
   )
 }
