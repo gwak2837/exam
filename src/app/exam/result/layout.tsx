f48d8f1b42c0e42c4beb80f43ca65019c0478ac2
Taeuk Gwak <gwak2837@kakao.com>
2023-10-28 00:13:02 +0900

    feat: 결과 페이지 디자인 중

---
+++ src/app/exam/result/layout.tsx
@@ -0,0 +1,16 @@
+import { LayoutProps } from '@/common/types'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Layout({ children }: LayoutProps) {
+  return (
+    <div className="mx-auto flex min-h-[100dvh] max-w-screen-lg flex-col">
+      <div className="sticky top-0 flex items-end justify-center gap-4 border-b border-gray-200 p-4 backdrop-blur sm:px-8 md:px-12 lg:border-b-0 lg:px-16">
+        <Link href="/" className="flex place-items-center gap-2 whitespace-nowrap p-4">
+          By <Image src="/bdsm-logo.svg" alt="bdsm-logo" width={24} height={24} priority />
+        </Link>
+      </div>
+      {children}
+    </div>
+  )
+}
--- src/app/exam/result/page.tsx
+++ src/app/exam/result/page.tsx
@@ -7,7 +7,7 @@ const Answers = dynamic(() => import('@/app/exam/result/Answers'), { ssr: false 
 
 export default async function Page({ params, searchParams }: PageProps) {
   return (
-    <main className="min-h-[100dvh] p-4 sm:p-8 md:p-16 lg:p-24">
+    <main className="flex grow flex-col p-4 sm:p-8 md:p-12 lg:p-16">
       <pre className="overflow-x-scroll">{JSON.stringify({ params, searchParams }, null, 2)}</pre>
       <Answers />
     </main>
