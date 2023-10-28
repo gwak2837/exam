335afbeeeafd30df8b4fdca19f8eb82540093d7b
Taeuk Gwak <gwak2837@kakao.com>
2023-10-28 13:15:35 +0900

    feat: disabled 시 popover 추가

--- src/app/exam/[questionCount]/이동Button.tsx
+++ src/app/exam/[questionCount]/이동Button.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useAnswerStore } from '@/app/exam/[questionCount]/zustand'
 import Modal from '@/components/atoms/Modal'
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
 import { MouseEvent, ReactNode, useState } from 'react'
@@ -14,14 +15,19 @@ export default function 이동Button({ children, add }: Props) {
   const searchParams = useSearchParams()
   const router = useRouter()
 
+  const { answers } = useAnswerStore()
+
   const questionCount = +params.questionCount
   const questionIndex = +(searchParams.get('i') ?? 1)
-  const disabled = questionIndex + add < 1
   const isSubmit = children === '다음' && questionCount === questionIndex
+  const 답안 = Object.values(answers[questionCount] ?? {})
+  const isFirstQuestion = questionIndex + add < 1
+  const hasUnsolvedQuestion = isSubmit && (답안.length !== questionCount || 답안.some((답안) => 답안.length === 0))
+  const disabled = isFirstQuestion || hasUnsolvedQuestion
 
   const [showModal, setShowModal] = useState(false)
 
-  function handle제출Click(e: MouseEvent) {
+  function handleButtonClick(e: MouseEvent) {
     if (disabled) return e.preventDefault()
 
     if (isSubmit) {
@@ -31,15 +37,31 @@ export default function 이동Button({ children, add }: Props) {
     }
   }
 
+  function handle좋아요ButtonClick() {
+    router.push(`/exam/result?examId=${questionCount}`)
+  }
+
   return (
     <>
-      <button
-        disabled={disabled}
-        className="transition-color rounded-lg bg-gray-300 px-4 py-3 text-sm text-gray-700 duration-300 hover:bg-gray-400/50 disabled:text-gray-400 disabled:hover:bg-gray-300"
-        onClick={handle제출Click}
-      >
-        {isSubmit ? '제출' : children}
-      </button>
+      <div className="relative">
+        <button
+          disabled={disabled}
+          className="transition-color peer whitespace-nowrap rounded-lg bg-gray-300 px-4 py-3 text-sm text-gray-700 duration-300 hover:bg-gray-400/50 disabled:text-gray-400 disabled:hover:bg-gray-300"
+          onClick={handleButtonClick}
+        >
+          {isSubmit ? '제출' : children}
+        </button>
+        {isSubmit && (
+          <div
+            className="max-w-64 pointer-events-none absolute left-full top-full translate-x-[-100%] p-2 text-sm text-gray-500 opacity-0 transition-opacity duration-300 hover:pointer-events-auto hover:opacity-100 peer-hover:peer-disabled:pointer-events-auto peer-hover:peer-disabled:opacity-100"
+            role="tooltip"
+          >
+            <div className="h-full w-full rounded-lg border border-gray-200 bg-white p-4 shadow">
+              <h3 className="whitespace-nowrap">모든 문제를 풀어주세요</h3>
+            </div>
+          </div>
+        )}
+      </div>
       <Modal open={showModal} onClose={() => setShowModal(false)} showCloseButton showDragButton>
         <div className="rounded-lg bg-white p-4 shadow-xl">
           <h3 className="my-1 text-lg font-medium">제출하기</h3>
@@ -47,7 +69,7 @@ export default function 이동Button({ children, add }: Props) {
           <div className="flex justify-end gap-2">
             <button
               className="transition-color w-20 rounded-lg bg-violet-200 px-4 py-2 text-sm text-violet-700 duration-300 hover:bg-violet-300"
-              onClick={() => router.push('/exam/result')}
+              onClick={handle좋아요ButtonClick}
             >
               좋아요
             </button>
