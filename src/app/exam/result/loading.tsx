2dec13a69b965eff39849dc3be7db1a5e60e9860
Taeuk Gwak <gwak2837@kakao.com>
2023-10-28 13:16:06 +0900

    chore: 채점하기 기능 만들기

---
+++ src/app/exam/result/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <main className="flex grow flex-col p-4 sm:p-8 md:p-12 lg:p-16">
+      <div role="status" className="max-w-sm animate-pulse">
+        <div className="mb-4 h-8 w-48 rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <div className="mb-2.5 h-4 max-w-[360px] rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <div className="mb-2.5 h-4 rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <div className="mb-2.5 h-4 max-w-[330px] rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <div className="mb-2.5 h-4 max-w-[300px] rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <div className="h-4 max-w-[360px] rounded-full bg-gray-100 dark:bg-gray-800"></div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </main>
+  )
+}
