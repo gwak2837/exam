0bd0443464ae7358d90191b1d484b670f0e7c279
Taeuk Gwak <gwak2837@kakao.com>
2023-10-29 17:34:18 +0900

    feat: 푼 문제도 진행바에 표시해주기

--- src/app/exam/[questionCount]/ProgressBar.tsx
+++ src/app/exam/[questionCount]/ProgressBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
+import { useAnswerStore, useExamStore } from '@/app/exam/[questionCount]/zustand'
 
 export default function ProgressBar() {
   const params = useParams()
@@ -11,19 +12,32 @@ export default function ProgressBar() {
 
   const router = useRouter()
 
+  const { answers } = useAnswerStore()
+
+  const { exams } = useExamStore()
+  const 문제Ids = exams[questionCount].map((q) => q.id)
+  const 답안Ids = answers[questionCount]
+
   return (
     <div className="relative grid h-full w-full grid-cols-[repeat(auto-fit,minmax(1px,1fr))] rounded-full bg-white">
       <div
-        className="absolute inset-0 z-10 h-full w-full  bg-violet-100 transition-transform"
+        className="absolute inset-0 z-10 h-full w-full  bg-violet-100 transition-transform duration-300"
         style={{ transform: `translateX(${(100 * questionIndex) / questionCount - 100}%)` }}
       />
       {[...Array(questionCount)].map((_, i) => (
         <button
           key={i}
-          className="relative z-10 transition-colors hover:bg-violet-300"
+          className={`relative z-10 transition-colors hover:bg-violet-300 ${
+            checked[String(답안Ids[문제Ids[i]]?.length > 0)]
+          }`}
           onClick={() => router.replace(`?i=${+i + 1}`)}
         />
       ))}
     </div>
   )
+}
+
+const checked: Record<string, string> = {
+  true: '!bg-violet-400',
+  false: '',
 }
