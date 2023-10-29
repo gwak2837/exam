479d63be14dca8208621284c24c9b1624ec1f17c
Taeuk Gwak <gwak2837@kakao.com>
2023-10-29 13:07:49 +0900

    feat: 채점 api 추가

--- src/app/api/question/result/route.ts
+++ src/app/api/question/result/route.ts
@@ -1,12 +1,34 @@
+import { exam } from '@/common/exam'
+
+export const runtime = 'edge'
+
+type Answers = Record<string, string[]>
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
-  const res = await fetch(`https://data.mongodb-api.com/product/id`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const product = await res.json()
 
-  return Response.json({ product })
+  const answers = Array.from(searchParams.keys()).reduce<Answers>((answers, questionId) => {
+    answers[questionId] = searchParams.getAll(questionId)
+    return answers
+  }, {})
+
+  const { 정답개수, 상세 } = Object.entries(answers).reduce(
+    ({ 정답개수, 상세 }, [문제Id, 답안]) => {
+      const 문제 = exam.find((문제) => 문제.id === 문제Id)
+      const 해설 = 문제?.해설 ?? ''
+      const 정답 = 문제?.정답?.sort() ?? []
+      const isCorrect = 정답.toString() === 답안.sort().toString()
+      return {
+        정답개수: isCorrect ? 정답개수 + 1 : 정답개수,
+        상세: { ...상세, [문제Id]: { 답안, 정답, 해설, isCorrect } },
+      }
+    },
+    { 정답개수: 0, 상세: {} },
+  )
+
+  return Response.json({
+    문제개수: Object.keys(answers).length,
+    정답개수,
+    상세,
+  })
 }
