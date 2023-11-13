d73eabd9d959cb89d96acc5cfa2b563bd456c9fc
Taeuk Gwak <gwak2837@kakao.com>
2023-11-13 09:49:32 +0900

    feat: page를 server 컴포넌트로 변경

--- src/app/exam/result/page.tsx
+++ src/app/exam/result/page.tsx
@@ -1,25 +1,10 @@
-'use client'
-
-import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
-import useSWR from 'swr'
 
-import Loading from '@/app/exam/[questionCount]/loading'
-import { ResultFallback } from '@/app/exam/result/Result'
-import 난이도선택하기Button from '@/app/exam/result/난이도선택하기Button'
-import 다시풀기Button from '@/app/exam/result/다시풀기Button'
-import 새로풀기Button from '@/app/exam/result/새로풀기Button'
-import { fetchJSON } from '@/util/swr'
+import ResultStatistics from '@/app/exam/result/ResultStatistics'
+import { type PageProps } from '@/common/types'
 
-const Result = dynamic(async () => await import('@/app/exam/result/Result'), { ssr: false, loading: ResultFallback })
-
-export default function Page() {
-  const searchParams = useSearchParams()
-
-  const { data, isLoading } = useSWR(`/api/question/result?${searchParams}`, fetchJSON)
-
-  if (isLoading) return <Loading />
+export default function Page({ searchParams }: PageProps) {
+  const 문제개수 = Array.from(new Set(Object.keys(searchParams))).length
 
   return (
     <main className="flex grow flex-col p-4 sm:p-8 md:p-12 lg:p-16">
@@ -28,22 +13,9 @@ export default function Page() {
         <Link href="/">https://bdsm.vercel.app</Link>
       </h5>
       <h4 className="my-8 text-center">
-        난이도: {get시험난이도(data.문제개수)} <span className="text-xs">({data.문제개수}개)</span>
+        난이도: {get시험난이도(문제개수)} <span className="text-xs">({문제개수}개)</span>
       </h4>
-      <h2 className="my-2 text-center text-2xl">
-        <b className="text-violet-900">{data.정답개수}</b> / {data.문제개수}
-      </h2>
-      <h3 className="my-2 flex items-center justify-center gap-2 text-lg">
-        등급: <b className="text-4xl text-violet-950">{get등급((100 * data.정답개수) / data.문제개수)}</b>
-      </h3>
-      <div className="h-8" />
-      <Result result={data} />
-      <div className="h-16" />
-      <div className="grid gap-4">
-        <다시풀기Button examId={data.문제개수} />
-        <새로풀기Button examId={data.문제개수} />
-        <난이도선택하기Button />
-      </div>
+      <ResultStatistics />
     </main>
   )
 }
@@ -59,16 +31,4 @@ function get시험난이도(questionCount: number) {
     default:
       return 'Custom'
   }
-}
-
-function get등급(score: number) {
-  if (score >= 95) return 'A+'
-  else if (score >= 90) return 'A'
-  else if (score >= 85) return 'B+'
-  else if (score >= 80) return 'B'
-  else if (score >= 75) return 'C+'
-  else if (score >= 70) return 'C'
-  else if (score >= 65) return 'D+'
-  else if (score >= 60) return 'D'
-  else return 'F'
 }
