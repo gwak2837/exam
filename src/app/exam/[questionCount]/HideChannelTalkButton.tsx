3865f69404e5860e100ac3df5eafbd1b262b4e2e
Taeuk Gwak <gwak2837@kakao.com>
2023-11-14 23:36:40 +0900

    fix: 채널톡 관련 element 모두 숨기기

--- src/app/exam/[questionCount]/HideChannelTalkButton.tsx
+++ src/app/exam/[questionCount]/HideChannelTalkButton.tsx
@@ -8,17 +8,14 @@ export default function HideChannelTalkButton() {
   useEffect(() => {
     void (async () => {
       const element = await waitForElement(document, '#ch-plugin-entry > div')
-      const element2 = element.shadowRoot?.querySelector('#ch-shadow-root-wrapper')
-      if (!element2) return
-
-      const channelTalkButton = await waitForElement(element2, '#ch-shadow-root-wrapper > div')
-      channelTalkButton?.setAttribute('style', 'display: none')
+      const channelTalkElement = element.shadowRoot?.querySelector('#ch-shadow-root-wrapper')
+      channelTalkElement?.setAttribute('style', 'display: none')
     })()
 
     return () =>
       document
         .querySelector('#ch-plugin-entry > div')
-        ?.shadowRoot?.querySelector('#ch-shadow-root-wrapper > div')
+        ?.shadowRoot?.querySelector('#ch-shadow-root-wrapper')
         ?.removeAttribute('style')
   }, [])
 
