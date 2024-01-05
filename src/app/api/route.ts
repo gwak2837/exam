e2a9980f59d7b10229722cb638d042c02c9b6560
Logan Gwak <logan.gwak@ridi.com>
2024-01-05 14:15:42 +0900

    fix: edge 빌드 오류

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,5 @@
 import { pool } from '@/app/api/postgres'
 
-export const runtime = 'edge'
-
 export async function GET() {
   const { rows } = await pool.query("SELECT 'Hello world!'")
   return Response.json({ data: rows })
