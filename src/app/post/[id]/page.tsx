b6372d776e6149c3cfdf77519ac489069445ebeb
Logan Gwak <logan.gwak@ridi.com>
2024-01-09 20:37:19 +0900

    refactor: postId -> id

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -11,9 +11,16 @@ const TinymceViewer = dynamic(async () => await import('@/components/TinymceView
 })
 
 export default async function Page({ params, searchParams }: PageProps) {
+  const postId = params.id
+
+  const response = await fetch(`http://localhost:3000/api/post/${postId}`)
+  const publicPost = await response.json()
+
+  console.log('👀 ~ publicPost:', publicPost)
+
   return (
     <main className="min-h-[100dvh] p-4 sm:p-8 md:p-16 lg:p-24">
-      <TinymceViewer initialValue="<p>Hello world</p><p>Hello world2</p>" />
+      <TinymceViewer initialValue={publicPost.content} />
     </main>
   )
 }
