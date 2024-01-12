8a5809685ff23cb7b1cfcd44ac28341b060b3e47
Logan Gwak <logan.gwak@ridi.com>
2024-01-13 01:02:52 +0900

    WIP: test case 작성 중

--- src/app/api/post/[id]/comment/route.test.ts
+++ src/app/api/post/[id]/comment/route.test.ts
@@ -1,15 +1,38 @@
-import { createOAuthUser, deleteOAuthUser } from '../../../test/user/util'
-
 import { GET } from '@/app/api/post/[id]/comment/route'
 import prisma from '@/app/api/prisma'
+import { createOAuthUser, deleteOAuthUser } from '@/database/User'
 
 beforeAll(async () => {
   await createOAuthUser({})
 })
 
 describe('GET /api/post/[id]/comment', () => {
-  test('One comments', async () => {
+  test('One comment', async () => {
     // await GET({ url: 'http://.', user: { id: '123' } } as any, { params: { id: '1' } })
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('One comment with referring post', async () => {
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('One comment with one reply', async () => {
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('One comment with many replies', async () => {
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('Many comments', async () => {
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('Many comments with one reply', async () => {
+    await expect(Promise.resolve(1)).resolves.toEqual(1)
+  })
+
+  test('Many comments with many replies', async () => {
     await expect(Promise.resolve(1)).resolves.toEqual(1)
   })
 })
